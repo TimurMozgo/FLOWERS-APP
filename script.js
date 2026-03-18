928d18b0f035196b65f354c63fb64636aa3d2239
Timur Mozgo <timurtamerlan2612@gmail.com>
2026-03-18 18:07:40 +0200

    add grid and icons

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 function showShop() {
-    console.log("Кнопка нажата");
     document.getElementById('welcome-screen').style.display = 'none';
     document.getElementById('shop-screen').style.display = 'flex';
 }
 
-function order(item) {
+// Теперь функция принимает два параметра: item (название) и price (число)
+function order(item, price) {
     const webhookUrl = 'https://tiktiok.xyz/webhook-test/e9d8d207-1c6b-44c2-a053-c41479cb32e6';
 
     fetch(webhookUrl, {
@@ -15,11 +15,12 @@ function order(item) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             product: item,
+            price: price, // Отправляем цену в n8n
             username: tg.initDataUnsafe.user?.username || "Grinzze"
         })
     });
 
-    tg.showConfirm(`Заказать ${item}?`, (confirmed) => {
+    tg.showConfirm(`Заказать ${item} за ${price} грн?`, (confirmed) => {
         if (confirmed) {
             tg.sendData(item);
             tg.close();
